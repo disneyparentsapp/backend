c3e47d1229438812eab14a30951ffa6393e7e357
McNultyPT <mcnultypt@gmail.com>
2019-03-18 12:55:38 -0700

    creating post request to register a user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../auth/auth-model.js');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 12);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Error registering the user.' });
+        });
+});
 
 module.exports = router;

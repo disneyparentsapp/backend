898a4c836ef807d49e06b976f9de5e9acbff23b0
McNultyPT <mcnultypt@gmail.com>
2019-03-19 08:46:45 -0700

    creating posts router

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const authRouter = require('../auth/auth-router.js');
+const postsRouter = require('../posts/posts-router.js');
 
 const server = express();
 
@@ -11,5 +12,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/', authRouter);
+server.use('/', postsRouter);
 
 module.exports = server;

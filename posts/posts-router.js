a9c50bf81b44bea8083f39a17263c692a04b351d
McNultyPT <mcnultypt@gmail.com>
2019-03-19 09:04:53 -0700

    creating posts router with get request to get all posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+const db = require('../data/dbConfig.js');
+
+router.get('/posts', (req, res) => {
+    db('posts')
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The posts could not be retrieved.' });
+        })
+});
+
 module.exports = router;

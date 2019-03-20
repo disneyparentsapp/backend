8ce8870f1ae4ccc72c5574f293d7197f15b3683e
McNultyPT <mcnultypt@gmail.com>
2019-03-20 16:59:59 -0700

    finishing post request tests on posts endpoint

--- posts/posts-router.spec.js
+++ posts/posts-router.spec.js
@@ -6,66 +6,69 @@ const server = require('../api/server.js');
 
 describe('posts-router.js', () => {
 
-        describe('GET /posts', () => {
-            it('should return 200 OK', async () => {
-               const res = await request(server).get("/posts");
+    describe('GET /posts', () => {
+        it('should return 200 OK', async () => {
+            const res = await request(server).get("/posts");
 
-               console.log(res.body);
-               expect(res.status).toBe(200);
-            });
-
-            it('should return JSON', async () => {
-                const res = await request(server).get('/posts');
-
-                expect(res.type).toBe('application/json');
-            });
-
-            it('should return an array', async () => {
-                const res = await request(server).get('/posts');
-
-                expect(Array.isArray(res.body)).toBe(true);
-            });
+            console.log(res.body);
+            expect(res.status).toBe(200);
         });
 
-        describe('POST /posts', () => {
+        it('should return JSON', async () => {
+            const res = await request(server).get('/posts');
 
-            afterEach(async () => {
-                await db('posts').truncate();
-            });
+            expect(res.type).toBe('application/json');
+        });
 
-            it('should return 201 OK', async () => {
-                const res = await request(server)
-                    .post('/posts')
-                    .send({
-                        name: 'test',
-                        location: 'test',
-                        kids: 1
-                    });
+        it('should return an array', async () => {
+            const res = await request(server).get('/posts');
 
-                expect(res.status).toBe(201);
-            });
+            expect(Array.isArray(res.body)).toBe(true);
+        });
+    });
 
-            it('should add post to the db', async () => {
-                let res = await request(server)
-                    .post('/posts')
-                    .send({
-                        name: 'test',
-                        location: 'test',
-                        kids: 1
-                    });
+    describe('POST /posts', () => {
 
-                res = await request(server).get('/posts');
+        afterEach(async () => {
+            await db('posts').truncate();
+        });
 
-                expect(res.body).toHaveLength(1)
-            });
+        it('should return 201 OK', async () => {
+             const res = await request(server)
+                .post('/posts')
+                .send({
+                    name: 'test',
+                    location: 'test',
+                    kids: 1
+                });
 
-            it('should return JSON', async () => {
-                const res = await request(server)
-                    .post('/posts')
-                    .send({
-                        name: 'test',
-                        location: 'test',
-                        kids: 1
-                    });
+            expect(res.status).toBe(201);
+        });
 
-//test
+        it('should add post to the db', async () => {
+            let res = await request(server)
+                .post('/posts')
+                 .send({
+                    name: 'test',
+                    location: 'test',
+                    kids: 1
+                });
+
+            res = await request(server).get('/posts');
+
+            expect(res.body).toHaveLength(1)
+        });
+
+        it('should return JSON', async () => {
+            const res = await request(server)
+                .post('/posts')
+                .send({
+                    name: 'test',
+                    location: 'test',
+                    kids: 1
+                });
+
+            expect(res.type).toBe('application/json');
+        });
+    });
+});

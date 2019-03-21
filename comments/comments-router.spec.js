47fd6b1359f39dad9beb731f862a23bc8897709c
McNultyPT <mcnultypt@gmail.com>
2019-03-21 12:55:59 -0700

    creating comments-router test file

---
+++ comments/comments-router.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+
+const db = require('../data/dbConfig.js');
+
+const server = require('../api/server.js');
+
+describe('comments-router.js' () => {
+
+    
+
+});

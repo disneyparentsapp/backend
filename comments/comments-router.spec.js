ef4703e2bd1040cad7b791b703199453baa23adc
McNultyPT <mcnultypt@gmail.com>
2019-03-21 14:09:24 -0700

    writing tests for post request on comments endpoint

--- comments/comments-router.spec.js
+++ comments/comments-router.spec.js
@@ -26,4 +26,49 @@ describe('comments-router.js', () => {
             expect(Array.isArray(res.body)).toBe(true);
         });
     });
+
+    describe('POST /comments', () => {
+
+        afterEach(async () => {
+            await db('comments').truncate();
+        });
+
+        it('should return 201 OK', async () => {
+             const res = await request(server)
+                .post('/comments')
+                .send({
+                    post_id: 1,
+                    name: 'test',
+                    comment: 'test'
+                });
+
+            expect(res.status).toBe(201);
+        });
+
+        it('should add post to the db', async () => {
+            let res = await request(server)
+                .post('/comments')
+                 .send({
+                    post_id: 1,
+                    name: 'test',
+                    comment: 'test'
+                });
+
+            res = await request(server).get('/comments');
+
+            expect(res.body).toHaveLength(1)
+        });
+
+        it('should return JSON', async () => {
+            const res = await request(server)
+                .post('/comments')
+                .send({
+                    post_id: 1,
+                    name: 'test',
+                    comment: 'test'
+                });
+
+            expect(res.type).toBe('application/json');
+        });
+    });
 });
